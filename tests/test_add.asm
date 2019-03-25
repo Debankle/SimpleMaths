7f8028f7b9b506d149e388dd07161b7317ede6cb
Oliver Pinel <oliver.debankle@gmail.com>
2019-03-25 18:22:34 +1000

    Test add function

---
+++ tests/test_add.asm
@@ -0,0 +1,31 @@
+bits 64
+
+global _main
+
+extern _add
+extern _exit
+extern _printf
+
+
+section .data
+    num1: equ 4
+    num2: equ 5
+
+    intFmt: db "%d", 0xa, 0
+
+section .text
+
+    _main:
+        push rbp
+        mov rbp, rsp
+
+        mov rdi, num1
+        mov rsi, num2
+        call _add
+
+        lea rdi, [ rel intFmt ]
+        mov rsi, rax
+        call _printf
+
+        mov rdi, 0
+        call _exit
