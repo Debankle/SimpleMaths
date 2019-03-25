5a4687412843bf5de788692154902c70cb49816b
Oliver Pinel <oliver.debankle@gmail.com>
2019-03-25 18:22:29 +1000

    Test absolute function

---
+++ tests/test_abs.asm
@@ -0,0 +1,27 @@
+bits 64
+
+global _main
+
+extern _abs
+extern _exit
+extern _printf
+
+section .data
+    num1: equ -10
+    intFmt: db "%d", 0xa, 0
+
+section .text
+
+    _main:
+        push rbp
+        mov rbp, rsp
+
+        mov rdi, num1
+        call _abs
+
+        lea rdi, [ rel intFmt ]
+        mov rsi, rax
+        call _printf
+
+        mov rdi, 0
+        call _exit
